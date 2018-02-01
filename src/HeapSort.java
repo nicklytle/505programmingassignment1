ef245b8c7eff797215571680830fda9528248c85
Bethany Janos <bpjanos@ncsu.edu>
2018-02-01 14:01:10 -0500

    Pair programming heapsort

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -17,18 +17,19 @@ public class HeapSort extends Sorter {
 	}
 
 	public static void main(String[] args) {
-		// ArrayList<Integer> arr = new ArrayList<>();
-		// arr.add(5);
-		// arr.add(4);
-		// arr.add(3);
+		 ArrayList<Integer> arr = new ArrayList<Integer>();
+		 arr.add(4);
+		 arr.add(3);
 		// arr.add(-100);
 		// arr.add(8);
 		// arr.add(0);
-		// arr.add(3);
+		 arr.add(2);
 		// arr.add(-8);
-		// // arr.add(1);
+		 arr.add(1);
+		 
+		 integerList = arr;
 		// integerList = arr;
-		read();
+		//read();
 		startTime = System.nanoTime();
 		heapSort(integerList);
 		endTime = System.nanoTime();
@@ -40,7 +41,7 @@ public class HeapSort extends Sorter {
 		heapSize = integerList.size();
 		int j = (int) Math.floor(integerList.size() / 2);
 		for (int i = (int) Math.floor(integerList.size() / 2); i >= 0; i--) {
-			MaxHeapify(i, integerList);
+			MaxHeapifybutWithoutComparisons(i, integerList);
 		}
 	}
 
@@ -58,11 +59,41 @@ public class HeapSort extends Sorter {
 		if (ri <= heapSize - 1) {
 			right = integerList.get((2 * index) + 1);
 		}
-		if (comp.compare(left, max) > 0) {
+		if (li <= heapSize - 1&&comp.compare(left, max) > 0) {
 			max = left;
 			switchindex = li;
 		}
-		if (comp.compare(right, max) > 0) {
+		if (ri <= heapSize - 1&&comp.compare(right, max) > 0) {
+			max = right;
+			switchindex = ri;
+		}
+		if (switchindex != index) {
+			integerList.set(index, max);
+			integerList.set(switchindex, inV);
+			MaxHeapify(switchindex, integerList);
+		}
+
+	}
+	
+	public static void MaxHeapifybutWithoutComparisons(int index, ArrayList<Integer> integerList) {
+		int li = 2 * index;
+		int ri = 2 * index + 1;
+		int switchindex = index;
+		Integer left = Integer.MIN_VALUE;
+		Integer right = Integer.MIN_VALUE;
+		Integer inV = integerList.get(index);
+		Integer max = inV;
+		if (li <= heapSize - 1) {
+			left = integerList.get(2 * index);
+		}
+		if (ri <= heapSize - 1) {
+			right = integerList.get((2 * index) + 1);
+		}
+		if (left > max) {
+			max = left;
+			switchindex = li;
+		}
+		if (right > max) {
 			max = right;
 			switchindex = ri;
 		}
