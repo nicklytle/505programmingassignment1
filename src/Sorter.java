020de6ac99f850e49180f741950e1ca36645d2e9
Bethany Janos <bpjanos@ncsu.edu>
2018-02-02 09:58:27 -0500

    Added a line to make data collection easier

--- src/Sorter.java
+++ src/Sorter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 //Abstract Class for Sorting Algorithms. Each 
 public abstract class Sorter {
-	
+
 	static ArrayList<Integer> integerList;
 	static Scanner in;
 	static int numItems;
@@ -14,24 +14,25 @@ public abstract class Sorter {
 	static double endTime;
 	static Comparator<Integer> comp = new Counter();
 
-	
-	public static void read(){
+	public static void read() {
 		in = new Scanner(System.in); // read in from Standard Input
-		integerList = new ArrayList<Integer>(); //instantiate list
-		numItems = Integer.parseInt(in.nextLine().replaceAll("[\\D]", ""));//read number of items
-		for(int i = 0; i < numItems; i++){ //for number of items
-			integerList.add(in.nextInt());//add item to list
+		integerList = new ArrayList<Integer>(); // instantiate list
+		numItems = Integer.parseInt(in.nextLine().replaceAll("[\\D]", ""));// read number of items
+		for (int i = 0; i < numItems; i++) { // for number of items
+			integerList.add(in.nextInt());// add item to list
 		}
 		in.close();
 	}
-	
-	public static void write(ArrayList<Integer> integerList){
-		for(Integer g: integerList){
+
+	public static void write(ArrayList<Integer> integerList) {
+		for (Integer g : integerList) {
 			System.out.println(g.toString());
 		}
-		//Print to Standard Error the runtime (expressed in milliseconds) and the number of comparisons
-		System.err.println("runtime,"+((int)runtime/1000000));
-		System.err.println("comparisons,"+numComparisons);
+		// Print to Standard Error the runtime (expressed in milliseconds) and the
+		// number of comparisons
+		System.err.println("runtime," + ((int) runtime / 1000000));
+		System.err.println("comparisons," + numComparisons);
+//		System.err.println((int) runtime / 1000000 + "," + numComparisons);
 	}
-	
+
 }
